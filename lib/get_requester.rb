ffe3341e169b392b5e67ba4940e7d45340b14686
michael thomas <michael.thomas2014@gmail.com>
2020-05-25 16:33:50 +0000

    Automatically backed up by Learn

--- lib/get_requester.rb
+++ lib/get_requester.rb
@@ -1,1 +1,27 @@
 # Write your code here
+require 'uri'
+
+
+
+
+class CetRequester
+  
+  
+  def initialize(url)
+    @url = url
+  end
+  
+  def get_response_body
+    open(@url)
+    response = HTTP::
+    response.body
+    
+  end
+  
+  def parse_json
+    
+  end
+  
+  
+  
+end
